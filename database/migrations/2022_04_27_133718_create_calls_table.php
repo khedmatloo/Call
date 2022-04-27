df9171db5c37e8dd42ed21886c21bd9764efe97b
Mehdi Khedmatloo <khedmatloom@gmail.com>
2022-04-27 18:31:29 +0430

    tables migration

---
+++ database/migrations/2022_04_27_133718_create_calls_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('user_type');
+            $table->integer('admin_id');
+            $table->integer('order_id');
+            $table->string('category');
+            $table->string('subcategory');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calls');
+    }
+};
